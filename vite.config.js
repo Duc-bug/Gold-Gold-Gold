989322da0af7c2a69aaeb4e857cbce3016749c4a
Duc Nguyen <nthienduc5@gmail.com>
2026-02-04 14:54:32 +0700

    update vite and package.json

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Tăng giới hạn cảnh báo lên 1000kB (tùy chọn)
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          charts: ['recharts'],
+          utils: ['date-fns', '@supabase/supabase-js']
+        }
+      }
+    }
+  }
 })
